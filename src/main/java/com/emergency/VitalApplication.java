803f86ea68d03f620e2a216ea196c720ef465396
giovane358 <giovane.etec@gmail.com>
2026-03-09 22:02:28 -0300

    Project base

---
+++ src/main/java/com/emergency/VitalApplication.java
@@ -0,0 +1,13 @@
+package com.emergency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalApplication.class, args);
+	}
+
+}
